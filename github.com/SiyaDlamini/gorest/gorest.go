d9bda557cae6a0473a95897ca8101f19e97b94d5
Prasad Jayashanka <prasadjayashanka@gmail.com>
2017-08-03 11:52:44 +0530

    test allowed all headers

--- github.com/SiyaDlamini/gorest/gorest.go
+++ github.com/SiyaDlamini/gorest/gorest.go
@@ -188,8 +188,11 @@ func (_ manager) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if origin := r.Header.Get("Origin"); origin != "" {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers",
-			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, SecurityToken, Tenant")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		/*
+			w.Header().Set("Access-Control-Allow-Headers",
+				"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, SecurityToken, Tenant")
+		*/
 	}
 	// Stop here if its Preflighted OPTIONS request
 	if r.Method == "OPTIONS" {
