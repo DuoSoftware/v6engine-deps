6b0b711d11a8db7f8fe2df6e19f2e52062ce46b7
Prasad Jayashanka <prasadjayashanka@gmail.com>
2017-08-03 12:01:08 +0530

    added new accepted tokens

--- github.com/SiyaDlamini/gorest/gorest.go
+++ github.com/SiyaDlamini/gorest/gorest.go
@@ -188,11 +188,8 @@ func (_ manager) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if origin := r.Header.Get("Origin"); origin != "" {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		/*
-			w.Header().Set("Access-Control-Allow-Headers",
-				"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, SecurityToken, Tenant")
-		*/
+		w.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, SecurityToken, Tenant, studio.crowd.tokenkey, JSESSIONID, atlassian.xsrf.token, cloud.session.token, Invitetype, Nounce")
 	}
 	// Stop here if its Preflighted OPTIONS request
 	if r.Method == "OPTIONS" {
